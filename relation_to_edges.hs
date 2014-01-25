f7f0291e43bb0d51842717e75a627a31b76e3186
Max Berson <maxberson@gmail.com>
2014-01-25 13:15:44 -0700

    Built the prototype out to the point where it can extract the academy names and lists of attendees

--- relation_to_edges.hs
+++ relation_to_edges.hs
@@ -1,25 +1,71 @@
 -- Our data is provided as CSV, so we need to import the utilities
 import Text.CSV
 
+-- Until I can find a suitable replacement, I'm using the O(n^2) NUB function
+import Data.List (nub)
+
 -- In testing, the CSV brought extra fields into each record. We only need the
--- leftmost 5 columns in this particular case
-purge_fields :: CSV -> CSV
-purge_fields records =
+-- leftmost 5 columns in this particular case.
+purge_columns :: CSV -> CSV
+purge_columns records =
   let columns = 5
   in map (take columns) records
+
+-- In testing, the CSV brought in some empty records. We only care about
+-- records that contain information.
+purge_empty :: CSV -> CSV
+purge_empty records = filter (\row -> (length row) > 1 ) records
+
+-- The file brings names in as two separate columns, but we're actually
+-- interested in first name-last name combinations to tell people apart. This
+-- utility consumes the parsed data from the file and produces a similar object
+-- with the second and third columns merged.
+concat_names :: CSV -> CSV
+concat_names records =
+  map (\[a, b, c, d, e] -> [a, b ++ " " ++ c, d, e]) records
+
+-- The input should be totally ready to process after stripping out the column
+-- headings, purging the extra columns and any blank records, then
+-- concatenating the name columns.
+clean_input :: CSV -> CSV
+clean_input records = concat_names $ purge_columns $ purge_empty $ tail records
+
+-- Once the input is cleaned, we'll abstract away the list structure, and refer
+-- to fields by their column heading.
+academy :: Record -> Field
+academy row = row !! 0
+name :: Record -> Field
+name row = row !! 1
+city :: Record -> Field
+city row = row !! 2
+company :: Record -> Field
+company row = row !! 3
+
+-- Determine if a record represents a person who attended a particular academy.
+attended_academy :: Field -> Record -> Bool
+attended_academy academy_name row = academy_name == academy row
+
+-- Given a table and the name of an academy, produce the list of people who
+-- attended the academy.
+attendees :: CSV -> Field -> Record
+attendees table academy_name =
+  map name $ filter (attended_academy academy_name) table
+
+-- Given a table, produce the list of academy names
+academies :: CSV -> Record
+academies table = nub $ map academy table
+
+-- Given a table, produce the list of tuples (A, B) where A is an academy name
+-- and B is the list of attendees
+academies_and_attendees :: CSV -> [ (Field, Record) ]
+academies_and_attendees table =
+  map (\academy_name -> (academy_name, attendees table academy_name)) $ academies table
 
 input_file_name :: FilePath
 input_file_name = "/home/max/transit_alliance/AcademyRoster_for_haskell.csv"
 
 output_file_name :: FilePath
 output_file_name = "/home/max/transit_alliance/AcademyNetworkGraph.csv"
-
-academy_col :: Int
-academy_col = 0
-city_col :: Int
-city_col = 3
-company_col :: Int
-company_col = 4
 
 -- The main action of this module is to bring in data from the CSV file, and
 -- either let the user know that there was an error, or find the edges within
@@ -31,37 +77,4 @@ main = do
   raw_CSV <- parseCSVFromFile input_file_name
   case raw_CSV of
     Left errmsg    -> putStrLn "CSV parse error:" >> print errmsg
-    Right contents -> find_edges $ purge_fields contents
-
--- The edges of the input graph include all those pairs of people who attended
--- the same Transit Alliance Academy, live in the same city, or work for the
--- same company. We can filter the input to find the lists of these people,
--- then use a permutation-finder to match each person on a list to each of the
--- others.
-find_edges :: CSV -> IO ()
-find_edges [] = return ()
-find_edges rows = do
-  let academies = nub $ map (\row -> row !! acad_col) rows
-  let cities = nub $ map (\row -> row !! city_col) rows
-  let companies = nub $ map (\row -> row !! company_col) rows
-  return ()
-
--- A pair of utility functions that allow us to extract all K-permutations from
--- an N-set. These functions were stolen from
--- http://stackoverflow.com/a/11766789, but I took a few minutes to evaluate
--- them by hand and add comments.
-
-select :: [a] -> [(a, [a])]
-select [] = []
--- The list comprehension below rearranges the input list (X:XS) into a new
--- list (Y:YS) such that Y =/= X and YS contains X. The final output is a list
--- of tuples [(L0, LS0), (L1, LS1) ... ] such that each L_i is distinct, and
--- each list LS_i contains those every element of (X:XS) expect for L_i.
-select (x:xs) = (x,xs) : [ (y,x:ys) | (y,ys) <- select xs ]
-
--- Return a list of every way to choose K elements from the input XS.
-perms :: Int -> [a] -> [[a]]
-perms 0 _ = [[]]
-perms k xs = do
-  (y, ys) <- select xs
-  fmap (y:) (perms (k - 1) ys)
+    Right contents -> print $ academies_and_attendees $ clean_input contents
