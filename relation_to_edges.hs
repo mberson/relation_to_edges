6b3c77c7ca7282df9c3bb53a4d32aed3aebccd0d
Max Berson <maxberson@gmail.com>
2014-01-31 00:41:10 -0700

    Can write node table and edge tables

--- relation_to_edges.hs
+++ relation_to_edges.hs
@@ -1,3 +1,6 @@
+-- I CAN'T FIGURE OUT WHAT THE FUCK TO CALL STUFF
+-- Ingredients : File name, contents, columns, rows, nodes, edges
+
 -- Our data is provided as CSV, so we need to import the utilities
 import Text.CSV
 
@@ -73,7 +76,9 @@ academies_and_attendee_pairs :: CSV -> [ (Field, CSV) ]
 academies_and_attendee_pairs table =
   map (\(a, b) -> (a, find_edges b)) $ academies_and_attendees table
 
--- Given a CSV table, produce the string that we'll write out to a file.
+-- Given a CSV table, produce the string that we'll write out to the node
+-- table. A list of people in the network, plus unique identifiers for each
+-- of them.
 node_table :: CSV -> String
 node_table roll_sheet =
   -- NB: As written below, this will not work if there are two people with
@@ -82,8 +87,34 @@ node_table roll_sheet =
     names = nub $ map name roll_sheet
     name_count = length names - 1
     id_to_name x = [show x, names !! x]
-    table = ["Id","Label"] : map id_to_name [0..name_count]
-  in printCSV table
+    header = ["Id", "Label"]
+    body = map id_to_name [0..name_count]
+    table = header : body
+  in
+    printCSV table
+
+add_ids_to :: CSV -> CSV
+add_ids_to table =
+  let
+    row_count = length table - 1
+    update row_num = show row_num : (table !! row_num)
+  in
+    map update [0..row_count]
+
+write_edge_files :: [(String, CSV)] -> IO ()
+write_edge_files ((key, table):rest) =
+  let
+    header = ["Id", "Source", "Target"]
+    file_name = "/tmp/TransitAlliance_" ++ key ++ ".csv"
+    rows = add_ids_to table
+    file_contents = printCSV (header:rows)
+  in
+    do
+      changes <- writeFile file_name file_contents
+      write_edge_files rest
+write_edge_files [] = do
+  return ()
+  
 
 input_file :: FilePath
 input_file = "/home/max/transit_alliance/AcademyRoster_for_haskell.csv"
@@ -91,18 +122,18 @@ input_file = "/home/max/transit_alliance/AcademyRoster_for_haskell.csv"
 node_table_file :: FilePath
 node_table_file = "/home/max/transit_alliance/Academy_node_table.csv"
 
-edge_table_file :: FilePath
-edge_table_file = "/home/max/transit_alliance/Academy_edge_table.csv"
-
+-- We can look at the top-level logic as several nested maps.
+-- The biggest thing is mapping over the file names of the various edge sets.
+-- For each file name, find those edges in the master table, and write them
+-- to their file.
 graph :: CSV -> IO ()
 graph table =
   let
-    sheet = clean_input table
+    master = clean_input table
   in
     do
-      writeFile node_table_file $ node_table $ sheet
---    writeFile edge_table_file $ edge_table sheet
-    
+      writeFile node_table_file $ node_table master
+      write_edge_files $ academies_and_attendee_pairs master
 
 -- The main action of this module is to bring in data from the CSV file, and
 -- either let the user know that there was an error, or find the edges within
@@ -113,5 +144,3 @@ main = do
   case raw_CSV of
     Left errmsg      -> putStrLn "CSV parse error:" >> print errmsg
     Right roll_sheet -> graph roll_sheet
-
-
